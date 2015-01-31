c39f1a0cac1ed652dfb9a750e1b1fbf5a3669291
SMarone <smarone@gatech.edu>
2015-01-31 10:27:44 -0500

    Another test for the git commiting scheme in a linux terminal.

---
+++ PythHelloWorld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+print "Hello, world!"
+
+name = raw_input("What is your name?\n")
+if name == "Dolby":
+  print "That's the name I would have guessed!  How odd."
+  print "I still don't like you."
+else :
+  print "Really? It's  %s?" % name
+  print "I can't handle that horrible of a name, I think I will just call you  Dolby."
+  print "No one likes you Dolby. Not even your own children."
